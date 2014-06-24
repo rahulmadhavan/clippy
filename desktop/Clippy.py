1d94fdfe11067f9e14d01c395f918ec473afcf9d
rahulm <rahulm@flipkart.com>
2014-06-24 20:19:08 +0530

    removed ip address form clippy receiver binding

--- desktop/Clippy.py
+++ desktop/Clippy.py
@@ -69,7 +69,7 @@ class ClippyReceiver(threading.Thread):
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((MCAST_GRP, self._port))
+        sock.bind(('', self._port))
         mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
